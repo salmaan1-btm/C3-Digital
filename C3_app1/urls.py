0fe106771e2f6b37827b269a7e07e681980802f9
SahibBoyal <sahibpre@ualberta.ca>
2025-03-17 11:22:58 -0600

    creation of dashboard, sales, inventory and claims pages

--- C3_app1/urls.py
+++ C3_app1/urls.py
@@ -6,4 +6,7 @@ app_name = 'C3_app1'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('claims/', views.claims, name='claims'),
+    path('inventory/', views.inventory, name='inventory'),
+    path('sales_p/', views.sales_p, name='sales_p'),
 ]
--- C3_app1/views.py
+++ C3_app1/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
     """The home page for C3 App 1."""
     return render(request, 'C3_app1/index.html')
+
+def claims(request):
+    """The Claims page for C3 App 1."""
+    return render(request, 'C3_app1/claims.html')
+
+def inventory(request):
+    """The inventory page for C3 App 1."""
+    return render(request, 'C3_app1/inventory.html')
+
+def sales_p(request):
+    """The sales portal page for C3 App 1."""
+    return render(request, 'C3_app1/sales_p.html')
