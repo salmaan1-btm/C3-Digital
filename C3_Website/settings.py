248297213e971069661f74032949e0c26aece120
SahibBoyal <sahibpre@ualberta.ca>
2025-03-21 09:49:16 -0600

    Navbar Creation

--- C3_Website/settings.py
+++ C3_Website/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My Settings
+
+STATICFILES_DIRS = [join(BASE_DIR, 'static')]
 
 
 LOGIN_REDIRECT_URL = '/'
