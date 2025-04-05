34625b366455065515c9ab65d994f95e06e29678
sophiak01 <syk@ualberta.ca>
2025-04-04 23:11:33 -0600

    Made logo the default product image

--- Apps/C3_app1/models.py
+++ Apps/C3_app1/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+def default_product_image():
+    return 'static/images/logo.jpg'
 # Create your models here.
 
 class Dealership(models.Model):
@@ -14,7 +16,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to = 'products/', blank = True, null = True)
+    image = models.ImageField(upload_to = 'products/', blank = True)
 
     def __str__(self):
         return self.name
@@ -31,22 +33,17 @@ class Inventory(models.Model):
 
 class Sale(models.Model):
     # A sales entry
-    STATUS_CHOICES = [
-        ('completed', 'Completed'),
-        ('pending', 'Pending'),
-        ('cancelled', 'Cancelled'),
-    ]
+
     inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
     dealership = models.ForeignKey(Dealership, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     description = models.TextField(blank=True, null=True) 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateField(auto_now_add=True)
     
     def clean(self):
         """Ensure enough stock is available before completing a sale."""
-        if self.status == "completed" and self.inventory.quantity < self.quantity:
+        if self.inventory.quantity < self.quantity:
             raise ValidationError("Not enough stock available for this sale.")
         """Ensure product is from the right dealership"""
         if self.dealership != self.inventory.dealership:
@@ -56,16 +53,14 @@ class Sale(models.Model):
             raise ValidationError("Quantity must be greater than 0.")
     def save(self, *args, **kwargs):
         self.clean()
-        if self.status =="completed":
-            # Deduct stock only when the sale is completed
-            if self.inventory.quantity < self.quantity:
-                raise ValidationError("Not enough stock available.")
-            self.inventory.quantity -= self.quantity
-            self.inventory.save()
+        if self.inventory.quantity < self.quantity:
+            raise ValidationError("Not enough stock available.")
+        self.inventory.quantity -= self.quantity
+        self.inventory.save()
         super().save(*args, **kwargs)
     def __str__(self):
         # Return a string representation of the model
-        return f" {self.inventory.dealership.name} - {self.inventory.product.name} - {self.quantity} units - {self.get_status_display()}"
+        return f" {self.inventory.dealership.name} - {self.inventory.product.name} - {self.quantity} units"
     
 class Claim(models.Model):
     STATUS_CHOICES = [
