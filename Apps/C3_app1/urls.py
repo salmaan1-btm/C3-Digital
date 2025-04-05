561809d564b885b24aab0a9701a5cca50743dbb7
SahibBoyal <sahibpre@ualberta.ca>
2025-04-04 22:49:41 -0600

    Change Password form added

--- Apps/C3_app1/urls.py
+++ Apps/C3_app1/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'C3_app1'
 
@@ -22,4 +23,6 @@ urlpatterns = [
     path('inventory/dealership/<int:dealership_id>/', views.dealership_inventory, name='dealership_inventory'),
     path("support/", views.support_view, name="support"),
     path('plot/',views.plot,name='plot'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='C3_app1/password_change.html',success_url=reverse_lazy('C3_app1:password_change_done')),name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='C3_app1/password_change_done.html'), name='password_change_done'),
 ]
